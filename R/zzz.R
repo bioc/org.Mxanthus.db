dda27c558debb904cbbc17bf42fe4cb03ebffc4f
eduardoillueca <56613143+eduardoillueca@users.noreply.github.com>
2019-10-22 20:40:10 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@ org.Mxanthus.egORGANISM <- "Myxococcus xanthus DK 1622"
 
 
 getData <- function(){
-    ah <- AnnotationHub()
+    ah <- AnnotationHub::AnnotationHub()
     query(ah, c("Myxococcus xanthus DK 1622", "org.Mxanthus.eg.db"))[[1]]
 }
