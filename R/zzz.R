3fdbd4106c66f3d6ac46b09ef15776523f4b42b8
eduardoillueca <56613143+eduardoillueca@users.noreply.github.com>
2019-10-23 18:58:40 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,15 @@ org.Mxanthus.egORGANISM <- "Myxococcus xanthus DK 1622"
 
 
 getData <- function(){
-    ah <- AnnotationHub::AnnotationHub()
-    query(ah, c("Myxococcus xanthus DK 1622", "org.Mxanthus.eg.db"))[[1]]
+  
+  ah <- AnnotationHub::AnnotationHub()
+  return(query(ah, c("Myxococcus xanthus DK 1622", "org.Mxanthus.eg.db"))[[1]])
+  
+}
+
+.onLoad <- function(libname, pkgname){
+  
+  
+  makeActiveBinding("org.Mxanthus.eg.db", getData , .GlobalEnv)
+  
 }
