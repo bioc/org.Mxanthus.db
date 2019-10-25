7009f8e8149b082eb9f195af689c8aeb4f4e34da
eduardoillueca <56613143+eduardoillueca@users.noreply.github.com>
2019-10-25 22:01:04 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,20 @@
 org.MxanthusORGANISM <- "Myxococcus xanthus DK 1622"
 
+getData <- local({
+    ah75133 <- NULL
+    function(value) {
+        stopifnot(missing(value))
+        if (is.null(ah75133)) {
+            ah <- AnnotationHub::AnnotationHub()
+            ah75133 <<- ah[["AH75133"]]
+        }
+        ah75133
+    }
+})
 
-getData <- function(){
-  
-  ah <- AnnotationHub::AnnotationHub()
-  return(query(ah, c("Myxococcus xanthus DK 1622", "org.Mxanthus.db"))[[1]])
-  
-}
-
-.onLoad <- function(libname, pkgname){
-  
-  
-  makeActiveBinding("org.Mxanthus.db", getData , .GlobalEnv)
-  
+.onLoad <- function(libname, pkgname)
+{
+    ns <- asNamespace(pkgname)
+    makeActiveBinding("org.Mxanthus.db", getData , env=ns)
+    namespaceExport(ns, "org.Mxanthus.db")
 }
